d8af24e1bade1c9d7852d052a63d607e6d66630e
jenphyjohn <117083959@qq.com>
2018-12-29 08:34:00 +0800

    简写注解方法

--- src/main/java/com/power/demo/controller/BooksController.java
+++ src/main/java/com/power/demo/controller/BooksController.java
@@ -16,35 +16,40 @@ public class BooksController {
 
     @Autowired
     private BookService bookService;
-    @RequestMapping(value = "/addBooks", method = RequestMethod.POST)
+//    @RequestMapping(value = "/addBooks", method = RequestMethod.POST)
+    @PostMapping("/addBooks") // 等同于上一行,简写
     @ApiOperation("添加图书")
     public String addBooks(@RequestBody BooksVO books){
         bookService.addBooks(books);
         return "成功";
     }
 
-    @RequestMapping(value = "/selectBooks", method = RequestMethod.GET)
+//    @RequestMapping(value = "/selectBooks", method = RequestMethod.GET)
+    @GetMapping("/selectBooks") // 等同于上一行,简写
     @ApiOperation("查询所有图书")
     public List<BooksVO> selectAllBooks(){
        List<BooksVO> books =  bookService.selectAllBooks();
        return books;
     }
 
-    @RequestMapping(value = "/selectByName", method = RequestMethod.GET)
+//    @RequestMapping(value = "/selectByName", method = RequestMethod.GET)
+    @GetMapping("/selectByName") // 等同于上一行,简写
     @ApiOperation("根据书名查询图书")
     public BooksVO selectByName(@RequestParam String booksName){
         BooksVO books = bookService.selectByName(booksName);
         return books;
     }
 
-    @RequestMapping(value = "/changeBooks", method = RequestMethod.PUT)
+//    @RequestMapping(value = "/changeBooks", method = RequestMethod.PUT)
+    @PutMapping("/changeBooks") // 等同于上一行,简写
     @ApiOperation("根据id修改图书内容")
     public int changeBooks(@RequestBody BooksVO book){
         int a = bookService.changeBooks(book);
         return a;
     }
 
-    @RequestMapping(value = "/deleteBooks", method = RequestMethod.DELETE)
+//    @RequestMapping(value = "/deleteBooks", method = RequestMethod.DELETE)
+    @DeleteMapping("/deleteBooks") // 等同于上一行,简写
     @ApiOperation("根据id删除图书")
     public String deleteBooks(@RequestParam String bookId){
         bookService.deleteBooks(bookId);
